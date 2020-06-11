8f88d9b42d5ec68dd56b1441fa4e9d9c997d0191
Deepak Bhangale <40859279+db9714@users.noreply.github.com>
2020-06-11 10:36:10 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
+const publicPath = path.join(__dirname, ‘...’,’public’);
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+const port= process.env.PORT || 3000
+app.use(express.static(publicPath));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
 
 
 //app.use(express.static(__dirname + '/dist'));
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
-});
 app.get('*', function(req, res){
-  res.sendFile(__dirname + '/dist/index.html');
+res.sendFile(path.join(publicPath,’index.html’);
 });
+
